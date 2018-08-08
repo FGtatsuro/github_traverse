52b1dafc5eb61a37ca09b6aa1770e3655f484b6b
FGtatsuro <fujiistorage@gmail.com>
2018-08-08 18:10:34 +0900

    Add a sample route to handle json response.

--- app.py
+++ app.py
@@ -7,10 +7,19 @@ def hello_world(request):
     return Response('<h1>Hello World!</h1>')
 
 
+def json(request):
+    return Response(json={'key': 'value'})
+
+
 if __name__ == '__main__':
     with Configurator() as config:
+
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
+
+        config.add_route('json', '/json')
+        config.add_view(json, route_name='json')
+
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
